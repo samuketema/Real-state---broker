f93e801becca17e49b070428671bc3d687214585
Samuel Ketema <samuelketema01@gmail.com>
2026-01-15 15:34:59 +0300

    main widget with global theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:realstate/core/theme/app_palette.dart';
+import 'package:realstate/features/auth/presentation/pages/sign_in_page.dart';
+import 'package:realstate/features/auth/presentation/pages/sign_up_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,21 +13,52 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Real State',
-      home: const MyHomePage(),
-    );
-  }
-}
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: AppPalette.primary,
+            foregroundColor: Colors.white,
+            padding: EdgeInsetsDirectional.symmetric(vertical: 16),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+        scaffoldBackgroundColor: AppPalette.appBgColor,
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: AppPalette.textBoxColor,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none,
+          ),
+          contentPadding: const EdgeInsets.symmetric(
+            vertical: 14,
+            horizontal: 15,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: AppPalette.inActiveColor, width: 1.5),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(color: AppPalette.primary, width: 2.5),
+          ),
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(color: AppPalette.red, width: 2),
+          ),
 
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello Flutter!'),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(color: AppPalette.red, width: 2.5),
+          ),
+        ),
       ),
+      title: 'Real State',
+      home: const SignUpPage(),
     );
   }
 }
