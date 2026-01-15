b1cf3223eaeb8fb6c64d5d579673bddaf042cce9
Samuel Ketema <samuelketema01@gmail.com>
2026-01-15 15:35:34 +0300

    sign up page

--- lib/features/auth/presentation/pages/sign_up_page.dart
+++ lib/features/auth/presentation/pages/sign_up_page.dart
@@ -8,8 +8,37 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(
+              'assets/images/logo.png',
+              height: screenHeight * 0.18,
+            
+              fit: BoxFit.contain,
+            ),
+            Text(
+              textAlign: TextAlign.center,
+              "Welcome again to Betlink",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            const SizedBox(height: 40),
+            TextFormField(),
+            const SizedBox(height: 15),
+            TextFormField(),
+            const SizedBox(height: 25),
+            ElevatedButton(onPressed: () {}, child: Text("Sign Up")),
+          ],
+        ),
+      ),
+    );
   }
 }
