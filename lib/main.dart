f3a7e9a9b1968831a1b79d55051ef4fca65542af
Samuel Ketema <samuelketema01@gmail.com>
2026-01-17 22:37:02 +0300

    supabase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:realstate/core/theme/app_palette.dart';
+import 'package:realstate/core/secrets/app_secrets/app_secrets.dart';
+import 'package:realstate/core/theme/app_theme.dart';
 import 'package:realstate/features/auth/presentation/pages/sign_in_page.dart';
-import 'package:realstate/features/auth/presentation/pages/sign_up_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+ final supabase = await Supabase.initialize(url: AppSecrets.url,anonKey: AppSecrets.annonKey);
   runApp(const MyApp());
 }
 
@@ -14,51 +17,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: AppPalette.primary,
-            foregroundColor: Colors.white,
-            padding: EdgeInsetsDirectional.symmetric(vertical: 16),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-        ),
-        scaffoldBackgroundColor: AppPalette.appBgColor,
-        inputDecorationTheme: InputDecorationTheme(
-          filled: true,
-          fillColor: AppPalette.textBoxColor,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide.none,
-          ),
-          contentPadding: const EdgeInsets.symmetric(
-            vertical: 14,
-            horizontal: 15,
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: AppPalette.inActiveColor, width: 1.5),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide(color: AppPalette.primary, width: 2.5),
-          ),
-
-          errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide(color: AppPalette.red, width: 2),
-          ),
-
-          focusedErrorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide(color: AppPalette.red, width: 2.5),
-          ),
-        ),
-      ),
+      theme: AppTheme.themeMode,
       title: 'Real State',
-      home: const SignUpPage(),
+      home: const SignInPage(),
     );
   }
 }
