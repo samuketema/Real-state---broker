d995a3abe28c80ea565d3121f5b0167da96958ac
Samuel Ketema <samuelketema01@gmail.com>
2026-01-17 22:38:01 +0300

    secret folder

---
+++ lib/core/secrets/app_secrets/app_secrets.dart
@@ -0,0 +1,4 @@
+class AppSecrets {
+    static const String url = "https://ijjkvcbgxahcecjqxqak.supabase.co";
+    static const String annonKey = "sb_publishable_XhdsOAbL30rVZ3iuHWdowQ_PeF0mgOy";
+}  
