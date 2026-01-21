cd6c56da90c2dfbebc0c1031901606977b1e7f52
Samuel Ketema <samuelketema01@gmail.com>
2026-01-21 22:50:55 +0300

    bloc state management for auth feature

---
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:realstate/features/auth/domain/usecases/user_log_in.dart';
+import 'package:realstate/features/auth/domain/usecases/user_sign_up.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final UserSignUp _userSignUp;
+  final UserLogIn _userLogIn;
+  AuthBloc({required UserSignUp userSignUp ,required UserLogIn userLogIn})
+    : _userSignUp = userSignUp,
+    _userLogIn = userLogIn,
+      super(AuthInitial()) {
+    on<AuthSignUp>((event, emit) async {
+      final res = await _userSignUp.call(
+        UserSignUpParams(
+          name: event.name,
+          email: event.email,
+          password: event.password,
+        ),
+      );
+      res.fold((l) =>emit( AuthFailure(l.message)), (r) =>emit( AuthSuccess(r)));
+    });
+    on<AuthLogIn>((event, emit)async {
+      final res = await _userLogIn.call(UserLogInParams(email: event.email, password: event.password));
+      res.fold((l)=> emit(AuthFailure(l.message)), (r)=> emit(AuthSuccess(r)));
+    },);
+  }
+}
---
+++ lib/features/auth/presentation/bloc/auth_event.dart
@@ -0,0 +1,19 @@
+part of 'auth_bloc.dart';
+
+@immutable
+sealed class AuthEvent {}
+
+final class AuthSignUp extends AuthEvent {
+    final String name;
+    final String email;
+    final String password;
+
+  AuthSignUp({required this.name, required this.email, required this.password});
+}
+
+final class AuthLogIn extends AuthEvent{
+  final String email;
+  final String password;
+
+  AuthLogIn({required this.email, required this.password});
+}
