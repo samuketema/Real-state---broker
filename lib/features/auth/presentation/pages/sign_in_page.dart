0b03bbc1cab01e30081fa0954185664a654a9721
Samuel Ketema <samuelketema01@gmail.com>
2026-01-21 22:52:39 +0300

    authentication

--- lib/features/auth/presentation/pages/sign_in_page.dart
+++ lib/features/auth/presentation/pages/sign_in_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:realstate/core/theme/app_palette.dart';
+import 'package:realstate/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:realstate/features/auth/presentation/pages/sign_up_page.dart';
+import 'package:realstate/features/auth/presentation/widgets/auth_elevated_button.dart';
+import 'package:realstate/features/auth/presentation/widgets/box_field.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
@@ -8,36 +14,96 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Image.asset(
-              'assets/images/logo.png',
-              height: screenHeight * 0.18,
-            
-              fit: BoxFit.contain,
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthFailure){
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(state.toString())));
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoading){
+            return Center( child: CircularProgressIndicator(),);
+          }
+          return Padding(
+            padding: const EdgeInsets.all(10),
+            child: SingleChildScrollView(
+              child: Form(
+                key: formkey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(height: screenHeight * 0.18),
+                    Image.asset(
+                      'assets/images/logo.png',
+                      height: screenHeight * 0.18,
+                      fit: BoxFit.contain,
+                    ),
+                    Text(
+                      textAlign: TextAlign.center,
+                      "Welcome back to Betlink",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                    BoxField(
+                      hintText: "Enter your email",
+                      controller: emailController,
+                    ),
+                    const SizedBox(height: 15),
+                    BoxField(
+                      isPassword: true,
+                      hintText: "Ent er your password",
+                      controller: passwordController,
+                    ),
+                    const SizedBox(height: 25),
+                    AuthElevatedButton(text: "Sign In",onPressed: () {
+                      context.read<AuthBloc>().add(AuthLogIn(email: emailController.text.trim(), password: passwordController.text.trim()));
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Successfully loggedIn")));
+                    },),
+                    const SizedBox(height: 15),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const SignUpPage(),
+                          ),
+                        );
+                      },
+                      child: RichText(
+                        text: TextSpan(
+                          text: "Don't have an account? ",
+                          style: Theme.of(context).textTheme.titleMedium,
+                          children: [
+                            TextSpan(
+                              text: "Sign Up",
+                              style: Theme.of(context).textTheme.titleMedium
+                                  ?.copyWith(
+                                    color: AppPalette.primary,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-
-            Text(
-              textAlign: TextAlign.center,
-              "Welcome again to Betlink",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            ),
-            const SizedBox(height: 40),
-            TextFormField(),
-            const SizedBox(height: 15),
-            TextFormField(),
-            const SizedBox(height: 25),
-            ElevatedButton(onPressed: () {}, child: Text("Sign In")),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
--- lib/features/auth/presentation/pages/sign_up_page.dart
+++ lib/features/auth/presentation/pages/sign_up_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:realstate/core/theme/app_palette.dart';
+import 'package:realstate/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:realstate/features/auth/presentation/pages/sign_in_page.dart';
+import 'package:realstate/features/auth/presentation/widgets/auth_elevated_button.dart';
+import 'package:realstate/features/auth/presentation/widgets/box_field.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -8,36 +14,115 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
-  
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Image.asset(
-              'assets/images/logo.png',
-              height: screenHeight * 0.18,
-            
-              fit: BoxFit.contain,
+      appBar: AppBar(),
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthFailure) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text(state.message)));
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return Center(child: CircularProgressIndicator());
+          }
+          return Padding(
+            padding: const EdgeInsets.all(10),
+            child: SingleChildScrollView(
+              // allows scrolling if keyboard overlaps
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(height: screenHeight * 0.08),
+                  Image.asset(
+                    'assets/images/logo.png',
+                    height: screenHeight * 0.18,
+                    fit: BoxFit.contain,
+                  ),
+                  Text(
+                    textAlign: TextAlign.center,
+                    "Create your Betlink account",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  BoxField(
+                    hintText: "Enter your name",
+                    controller: nameController,
+                  ),
+                  const SizedBox(height: 15),
+                  BoxField(
+                    hintText: "Enter your email",
+                    controller: emailController,
+                  ),
+                  const SizedBox(height: 15),
+                  BoxField(
+                    isPassword: true,
+                    hintText: "Enter your password",
+                    controller: passwordController,
+                  ),
+                  const SizedBox(height: 25),
+                  AuthElevatedButton(
+                    text: "Sign Up",
+                    onPressed: () {
+                      context.read<AuthBloc>().add(
+                        AuthSignUp(
+                          name: nameController.text.trim(),
+                          email: emailController.text.trim(),
+                          password: passwordController.text.trim(),
+                        ),
+                      );
+                    },
+                  ),
+                  const SizedBox(height: 15),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) => SignInPage()),
+                      ); // go back to SignInPage
+                    },
+                    child: RichText(
+                      text: TextSpan(
+                        text: "Already have an account? ",
+                        style: Theme.of(context).textTheme.titleMedium,
+                        children: [
+                          TextSpan(
+                            text: "Sign In",
+                            style: Theme.of(context).textTheme.titleMedium
+                                ?.copyWith(
+                                  color: AppPalette.primary,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              textAlign: TextAlign.center,
-              "Welcome again to Betlink",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            ),
-            const SizedBox(height: 40),
-            TextFormField(),
-            const SizedBox(height: 15),
-            TextFormField(),
-            const SizedBox(height: 25),
-            ElevatedButton(onPressed: () {}, child: Text("Sign Up")),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
