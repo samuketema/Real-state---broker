8aec339700236e44083d15abd4f27949b41c2631
Samuel Ketema <samuelketema01@gmail.com>
2026-01-21 22:50:23 +0300

    depencency for auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:realstate/core/secrets/app_secrets/app_secrets.dart';
 import 'package:realstate/core/theme/app_theme.dart';
-import 'package:realstate/features/auth/data/datasources/auth_remote_datasource.dart';
-import 'package:realstate/features/auth/data/repositories/authrepositoryimpl.dart';
-import 'package:realstate/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:realstate/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:realstate/features/auth/presentation/pages/sign_in_page.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:realstate/init_dependencies.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final supabase = await Supabase.initialize(
-    url: AppSecrets.url,
-    anonKey: AppSecrets.annonKey,
-  );
-  final supabaseClient = supabase.client;
-
+  initDependencies();
   runApp(
     MultiBlocProvider(
       providers: [
         BlocProvider<AuthBloc>(
-          create: (_) => AuthBloc(
-            userSignUp: UserSignUp(
-              authRepository: Authrepositoryimpl(
-                authRemoteDataSource: AuthRemoteDatasourceImpl(
-                  supabase: supabaseClient,
-                ),
-              ),
-            ),
-          ),
+          create: (_) => sl<AuthBloc>()
         ),
       ],
       child: const MyApp(),
