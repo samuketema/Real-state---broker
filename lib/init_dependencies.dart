b513ca0a60c85d88fc38c73d6b060a5cac0968df
Samuel Ketema <samuelketema01@gmail.com>
2026-01-22 22:44:39 +0300

    dependency injection

---
+++ lib/init_dependencies.dart
@@ -0,0 +1,28 @@
+import 'package:get_it/get_it.dart';
+import 'package:realstate/core/secrets/app_secrets/app_secrets.dart';
+import 'package:realstate/features/auth/data/datasources/auth_remote_datasource.dart';
+import 'package:realstate/features/auth/data/repositories/authrepositoryimpl.dart';
+import 'package:realstate/features/auth/domain/repository/auth_repository.dart';
+import 'package:realstate/features/auth/domain/usecases/user_log_in.dart';
+import 'package:realstate/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:realstate/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initDependencies() async {
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.url,
+    anonKey: AppSecrets.annonKey,
+  );
+  sl.registerLazySingleton(() => supabase.client);
+  _initAUth();
+}
+
+void _initAUth() {
+  sl.registerFactory<AuthRemoteDatasource>(()=>AuthRemoteDatasourceImpl(supabase: sl()));
+  sl.registerFactory<AuthRepository>(()=>AuthRepositoryImpl(authRemoteDataSource: sl()));
+  sl.registerFactory(()=>UserSignUp(authRepository: sl()));
+  sl.registerFactory(() => UserLogIn(authReopsitory: sl()));
+  sl.registerLazySingleton(()=> AuthBloc(userSignUp: sl(), userLogIn: sl()));
+}
