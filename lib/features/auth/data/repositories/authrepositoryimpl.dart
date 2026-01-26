66c6e9b291d05f391cb330727791eb074a40e311
Samuel Ketema <samuelketema01@gmail.com>
2026-01-26 23:00:32 +0300

    depending in interface.

--- lib/features/auth/data/repositories/authrepositoryimpl.dart
+++ lib/features/auth/data/repositories/authrepositoryimpl.dart
@@ -2,23 +2,30 @@ import 'package:fpdart/fpdart.dart';
 import 'package:realstate/core/error/exception.dart';
 import 'package:realstate/core/error/failure.dart';
 import 'package:realstate/features/auth/data/datasources/auth_remote_datasource.dart';
+import 'package:realstate/features/auth/domain/entities/user.dart';
 import 'package:realstate/features/auth/domain/repository/auth_repository.dart';
 
-class Authrepositoryimpl implements AuthRepository {
-  final AuthRemoteDatasourceImpl authRemoteDataSource;
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDatasource authRemoteDataSource;
 
-  Authrepositoryimpl({required this.authRemoteDataSource});
+  AuthRepositoryImpl({required this.authRemoteDataSource});
   @override
-  Future<Either<Failure, String>> logInWithEmailPassword({
+  Future<Either<Failure, User>> logInWithEmailPassword({
     required String email,
     required String password,
-  }) {
-    // TODO: implement logInWithEmailPassword
-    throw UnimplementedError();
+  })async {
+    try {
+      final res = await authRemoteDataSource.logInWithEmailPassword(email: email, password: password);
+    return Right(res);
+    } on ServerException catch (e) {
+      return left(Failure(e.message));
+    }catch(e){
+      return left(Failure('Signup failed: $e'));
+    }
   }
 
   @override
-  Future<Either<Failure, String>> signUpWithEmailPassword({
+  Future<Either<Failure, User>> signUpWithEmailPassword({
     required String name,
     required String email,
     required String password,
