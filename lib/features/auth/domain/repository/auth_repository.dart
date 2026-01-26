209c03666971ccc1ebbbaba4086c28e87be8801c
Samuel Ketema <samuelketema01@gmail.com>
2026-01-26 23:04:09 +0300

    Return type string to User

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -1,13 +1,14 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:realstate/core/error/failure.dart';
+import 'package:realstate/features/auth/domain/entities/user.dart';
 
 abstract interface class AuthRepository {
-  Future<Either<Failure,String>> signUpWithEmailPassword({
+  Future<Either<Failure,User>> signUpWithEmailPassword({
     required String name,
     required String email,
     required String password
   });
-  Future<Either<Failure,String>> logInWithEmailPassword({
+  Future<Either<Failure,User>> logInWithEmailPassword({
     required String email,
     required String password
   });
